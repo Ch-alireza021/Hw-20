7fa8286b5d4064052e3e4b5b14870d01e52e607f
Alireza Chizari <ch.alireza021@gmail.com>
2023-11-15 10:56:57 +0330

    feat : base/button

---
+++ stop-watch/src/components/base/button/Button.jsx
@@ -0,0 +1,10 @@
+export const Button = ({backGround,text,onBtn}) => {
+ 
+    return (
+      <div>
+          <button onClick={onBtn} onDoubleClick={doubleClick} className={`${backGround} rounded-full w-[120px] h-[120px]`}>
+              {text}
+          </button>
+      </div>
+    )
+  }
