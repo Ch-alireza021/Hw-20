898d8c58dd1042da74b260dad139f73d91a1e8c4
Alireza Chizari <ch.alireza021@gmail.com>
2023-11-15 11:34:34 +0330

    feat :widget/stop watch

--- stop-watch/src/App.js
+++ stop-watch/src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
- 
+    <div className="w-full min-h-screen bg-[#3E3E3E] flex flex-col items-center text-white text-2xl">
+      {/* <StopWatchShow /> */}
     </div>
   );
 }
---
+++ stop-watch/src/components/widget/stopWatch/StopWatch.jsx
@@ -0,0 +1,93 @@
+// StopWatch.js
+import React, { useState, useEffect } from "react";
+import Button from "../../base/button/Button";
+import { calculateTimeDifference } from "../../../app/calculateTimeDifference";
+
+
+
+export default function StopWatch() {
+  const [time, setTime] = useState(false);
+  const [timeStop, setTimeStop] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
+  const [seconds, setSeconds] = useState("00");
+  const [minutes, setMinutes] = useState("00");
+  const [hours, setHours] = useState("00");
+  const [miliSeconds, setMiliSeconds] = useState("00");
+  const [laps, setLaps] = useState([]);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        const timeNow = new Date().getTime();
+        const differenceTime = calculateTimeDifference(time, timeNow);
+        setMiliSeconds(differenceTime.milliseconds);
+        setSeconds(differenceTime.seconds);
+        setMinutes(differenceTime.minutes);
+        setHours(differenceTime.hours);
+      }, 10);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const handleStart = () => {
+	if(!time){
+		const startStopWatch = new Date().getTime();
+		setTime(startStopWatch);
+	}else{
+		const Now = new Date().getTime();
+		setTime(prev=>prev+(Now-timeStop))
+	}
+	setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+	setTimeStop(new Date().getTime());
+  };
+
+  const handleLap = () => {
+    const lapTime = hours + ":" + minutes + ":" + seconds + "." + miliSeconds;
+	if(isRunning){
+		setLaps((prevLaps) => [...prevLaps, lapTime]);
+	}
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setMiliSeconds("00");
+    setSeconds("00");
+    setMinutes("00");
+    setHours("00");
+    setLaps([]);
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-10 mt-7">
+      <div className="text-[48px] w-fit">
+        {hours} : {minutes} : {seconds} . {miliSeconds}
+      </div>
+          <div className="w-full flex justify-evenly"  disabled={isRunning}>
+           
+            <Button
+              backGround={"bg-[#18a71e]"}
+              text={"START"}
+              onBtn={handleStart}
+            />
+            <Button
+              backGround={"bg-[#187CA7]"}
+              text={"LAP"}
+              onBtn={handleLap}
+            />
+            <Button
+              backGround={"bg-[#A71818]"}
+              text={isRunning ?"STOP":"RESET"}
+              onBtn={isRunning ? handleStop :handleReset}
+            />
+          </div>
+          {/* <Lap laps={laps} /> */}
+     
+    </div>
+  );
+}
