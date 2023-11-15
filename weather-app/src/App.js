f0732d1db847fa94f33f5cac180d35ec753ec618
Alireza Chizari <ch.alireza021@gmail.com>
2023-11-15 13:27:50 +0330

    style : date and loc

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import DateAndLoc from './components/base/dateAndLoc/DateAndLoc';
+import Search from './components/base/search/Search';
 
 function App() {
   return (
-    <div className="w-full min-h-screen bg-slate-600">
-
+    <div className="w-full min-h-screen bg-[#272E37] flex flex-col items-center">
+      <Search/>
+      <DateAndLoc day={"wedensday"} date={11}  loc={"tehran"} />
     </div>
   );
 }
---
+++ weather-app/src/components/base/search/Search.jsx
@@ -0,0 +1,13 @@
+const Search = (prps) => {
+  return (
+    <div className="w-[1000px] text-[20px] font-semibold text-white my-[47px]">
+      <input
+        type="search"
+        placeholder="search city"
+        className="w-full px-7 border-[#E7E7E7] border-2 rounded-[17px] h-[79px] outline-none bg-transparent"
+      />
+    </div>
+  );
+};
+
+export default Search;
